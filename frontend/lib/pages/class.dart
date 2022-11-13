2cf41f702a05ba410ffcefbf65ad14b98f236b8e
luizlcezario <luizlcezario@gmail.com>
2022-11-13 19:13:29 -0300

    menu sanduiche e logo

--- frontend/lib/pages/class.dart
+++ frontend/lib/pages/class.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import '../main.dart';
-import 'exercises.dart';
+import '../components/sanduiche.dart';
 
 class ClassPage extends StatefulWidget {
   const ClassPage({super.key});
@@ -44,45 +43,7 @@ class _ClassPageState extends State<ClassPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(161, 215, 233, 1),
-              ),
-              child: Text(
-                'LOGO',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-                leading: Icon(Icons.class_),
-                title: Text('Feedback'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyHomePage()),
-                  );
-                }),
-            ListTile(
-              leading: Icon(Icons.class_),
-              title: Text('Salas'),
-            ),
-            ListTile(
-                leading: Icon(Icons.data_exploration_rounded),
-                title: Text('Exercícios'),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ExercisesPage()));
-                }),
-          ],
-        ),
-      ),
+      drawer: Sanduiche(path: 'Salas'),
       appBar: AppBar(
         title: Text('Salas'),
       ),
@@ -136,25 +97,20 @@ class _ClassPageState extends State<ClassPage> {
                   children: e["student"].map<Widget>((aluno) {
                     return e['name'] == dropdownValue
                         ? Container(
-                            margin: EdgeInsets.only(right: 20, left: 20),
+                            margin:
+                                EdgeInsets.only(right: 20, left: 20, top: 20),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(161, 215, 233, 1),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                             child: Row(
                               children: [
-                                Container(
-                                  width: 100,
-                                  height: 100,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    color: Color.fromRGBO(161, 215, 233, 1),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      aluno["name"],
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                  ),
+                                CircleAvatar(
+                                  backgroundImage: AssetImage(aluno['img']),
                                 ),
                                 Container(
-                                  margin: EdgeInsets.only(top: 10),
+                                  margin: EdgeInsets.only(left: 20),
                                   child: Text(
                                     aluno["name"],
                                     style: TextStyle(color: Colors.black),
